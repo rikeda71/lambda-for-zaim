e0bf48075570901c18e53f4a7ebca6db856632b5
Ryuya Ikeda <rikeda71@gmail.com>
2020-03-15 17:12:16 +0900

    renamed

--- LambdaForZaim/src/main/java/lambdaForZaim/model/Money.java
+++ LambdaForZaim/src/main/java/lambdaForZaim/model/Money.java
@@ -8,6 +8,10 @@ public class Money {
   private int total;
   private Map<String, Integer> amountForCategory;
 
+  Money() {
+
+  }
+
   Money(int total, Map<String, Integer> amountForCategory) {
     this.total = total;
     this.amountForCategory = amountForCategory;
