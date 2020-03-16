c67925a0900d46d2397e884529db386648481c0a
Ryuya Ikeda <rikeda71@gmail.com>
2020-03-17 00:01:56 +0900

    update settings

--- build-for-start-api.sh
+++ build-for-start-api.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo 'execute `gradle clean build` and `copy zip file to ./`;'
-cd LambdaForZaim && gradle clean build && cp build/distributions/LambdaForZaim.zip ../lambdaForZaim.zip
+cd LambdaForZaim && gradle clean build && cp build/distributions/LambdaForZaim.zip ../lambdaForZaim.zip && cd ../
+cd ZaimToSlack && gradle clean build && cp build/distributions/ZaimToSlack.zip ../zaimToSlack.zip
